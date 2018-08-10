c58553abe9319d6a7dab034fab0d2f098545174c
tomiaJO <t.koncz@gmail.com>
2018-08-10 18:45:18 +0200

    First commit - comparing different models by AUC / ROC curve

---
+++ Functions/f_model_comparison.R
@@ -0,0 +1,15 @@
+f_model_comparison <- function (models, df_test) {
+  
+  pred_prob  <- predict.train(models[[1]], newdata = data_test, type = "prob")[, "Yes", drop = F]
+  names(pred_prob)[1] = names(models)[1]
+  
+  for(i in 2:length(models)) {
+    pred_prob  <- cbind(pred_prob, predict.train(models[[i]], newdata = df_test, type = "prob")[, "Yes", drop = F])
+    names(pred_prob)[i] = names(models)[[i]]
+  }
+  
+  p <- f_roc_create(pp = pred_prob, truth = df_test$injured)
+  a <- f_calc_auc(  pp = pred_prob, truth = df_test$injured)
+  
+  return(list("plot" = p, "auc" = a))
+}
