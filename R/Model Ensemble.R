244d4eabaa7009eadd0b1ca3852e805e18a89084
tomiaJO <t.koncz@gmail.com>
2018-08-12 19:19:49 +0200

    wip

---
+++ R/Model Ensemble.R
@@ -0,0 +1,86 @@
+## Import libraries
+library(tidyverse)
+library(caret)
+
+
+## Clean-up environment
+rm(list = ls())
+gc()
+
+## Import folder structure
+source("GlobalVariables.R")
+
+## Import performance evaulation functions
+source(paste(path_Functions, "f_calc_auc.R",         sep = "/"))
+source(paste(path_Functions, "f_roc_create.R",       sep = "/"))
+source(paste(path_Functions, "f_model_comparison.R", sep = "/"))
+
+## Load data_test
+data_train     <- readRDS(file = paste(path_Data, "data_train.RDS",       sep = "/"))
+data_test      <- readRDS(file = paste(path_Data, "data_test.RDS",        sep = "/"))
+
+## Load models
+## GLM
+# m_glm_no       <- readRDS(file = paste(path_Models, "m_glm_no.RDS",       sep = "/"))
+# m_glm_up       <- readRDS(file = paste(path_Models, "m_glm_up.RDS",       sep = "/"))
+m_glm_down     <- readRDS(file = paste(path_Models, "m_glm_down.RDS",     sep = "/"))
+# m_glm_smote    <- readRDS(file = paste(path_Models, "m_glm_smote.RDS",    sep = "/"))
+
+## GLMNet
+m_glmnet_down  <- readRDS(file = paste(path_Models, "m_glmnet_down.RDS",  sep = "/"))
+
+## RF
+m_rf_down      <- readRDS(file = paste(path_Models, "m_rf_down.RDS",      sep = "/"))
+
+## GBM --> 3.5 hours
+#m_gbm_down     <- readRDS(file = paste(path_Models, "m_gbm_down.RDS",     sep = "/"))
+
+## XGBOOST
+m_xgboost_down  <- readRDS(file = paste(path_Models, "m_xgboost_down.RDS", sep = "/"))
+
+
+#####################################
+## COMBINING MODELS
+
+pred_ensemble_train <- data.frame(
+      "injured"         = data_train$injured,
+      "glm"             = predict.train(m_glm_down, newdata = data_train, type = "prob")[, "Yes", drop = T],
+      "GLMNet"          = predict.train(m_glmnet_down, newdata = data_train, type = "prob")[, "Yes", drop = T],
+      "Random Forest"   = predict.train(m_rf_down, newdata = data_train, type = "prob")[, "Yes", drop = T],
+      "XGBOOST"         = predict.train(m_xgboost_down, newdata = data_train, type = "prob")[, "Yes", drop = T])
+
+cor(pred_ensemble_train[, c(2:5)])
+
+ctrl <- trainControl(method = "repeatedcv", 
+                     repeats = 5,
+                     classProbs = TRUE,
+                     summaryFunction = twoClassSummary)
+
+ctrl$sampling = "down"
+
+set.seed(93)
+m_ensemble_down <- train(injured ~ .,
+                    data      = pred_ensemble_train,
+                    method    = "glm",
+                    family    = "binomial",
+                    metric    = "ROC",
+                    trControl = ctrl)
+
+
+pred_ensemble_test <- data.frame(
+  "injured"         = data_test$injured,
+  "glm"             = predict.train(m_glm_down, newdata = data_test, type = "prob")[, "Yes", drop = T],
+  "GLMNet"          = predict.train(m_glmnet_down, newdata = data_test, type = "prob")[, "Yes", drop = T],
+  "Random Forest"   = predict.train(m_rf_down, newdata = data_test, type = "prob")[, "Yes", drop = T],
+  "XGBOOST"         = predict.train(m_xgboost_down, newdata = data_test, type = "prob")[, "Yes", drop = T])
+
+pp_ens <- predict.train(m_ensemble_down, newdata = pred_ensemble_test, type = "prob")[, "Yes", drop = T]
+pred_ensemble_test$Ensemble <- pp_ens
+
+pred_ensemble_test %>% head()
+
+f_roc_create (pred_ensemble_test[, c(2:6)], pred_ensemble_test$injured)
+f_calc_auc(  pred_ensemble_test[, c(2:6)], pred_ensemble_test$injured)
+
+
+ggsave(filename = "ROC_on_test.jpeg", device = "jpeg", dpi = 1600, width = 6, height = 4)
