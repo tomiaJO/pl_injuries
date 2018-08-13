248410b086fce5e5a2d8e5067f06a2e65059ce54
tomiaJO <t.koncz@gmail.com>
2018-08-13 18:25:25 +0200

    added conditional save for separate chart pieces

--- Functions/f_save_plots.R
+++ Functions/f_save_plots.R
@@ -1,21 +1,28 @@
-f_save_plots <- function(save_plots, p, n, title, w, h) {
+f_save_plots <- function(save_plots, p, n, title, w, h, save_all = FALSE) {
   
   f_conditional_ggsave(save = save_plots, 
-                       p = p$storyplot, 
-                       filepath = paste(path_Figures, "/", n, ". ", title, ".jpeg", sep = ""), 
-                       w = w, 
+                       p = p$grid, 
+                       filepath = paste(path_Figures, "/", n, "g. ", title, ".jpeg", sep = ""), 
+                       w = w * 1.75, 
                        h = h)
   
-  f_conditional_ggsave(save = save_plots, 
-                       p = p$ciplot, 
-                       filepath = paste(path_Figures, "/", n, "t. ", title, ".jpeg", sep = ""), 
-                       w = w, 
-                       h = h)
-  
-  title <- gsub(pattern = "rate", replacement = "length", x = title, ignore.case = T)
-  f_conditional_ggsave(save = save_plots, 
-                       p = p$ilenplot, 
-                       filepath = paste(path_Figures, "/", n, ". ", title, ".jpeg", sep = ""), 
-                       w = w, 
-                       h = h)
+  if(save_all) {
+    f_conditional_ggsave(save = save_plots, 
+                         p = p$storyplot, 
+                         filepath = paste(path_Figures, "/", n, ". ", title, ".jpeg", sep = ""), 
+                         w = w, 
+                         h = h)
+    
+    f_conditional_ggsave(save = save_plots, 
+                         p = p$ciplot, 
+                         filepath = paste(path_Figures, "/", n, "t. ", title, ".jpeg", sep = ""), 
+                         w = w, 
+                         h = h)
+    
+    f_conditional_ggsave(save = save_plots, 
+                         p = p$ilenplot, 
+                         filepath = paste(path_Figures, "/", n, ". ", title, ".jpeg", sep = ""), 
+                         w = w, 
+                         h = h)
+  }
 }
